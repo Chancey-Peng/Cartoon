f7485f3307bfeaf38827d90fac150f4ac8f83e8f
Chancey <474084332@qq.com>
2020-09-22 18:31:52 +0800

    分离抽出相关用户操作

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -2,3 +2,48 @@ from django.db import models
 
 # Create your models here.
 
+from datetime import datetime
+
+from apps.caricature.models import Caricature
+
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+
+
+class UserFav(models.Model):
+    """
+    用户收藏
+    """
+    user = models.ForeignKey(User, verbose_name="用户", on_delete=models.CASCADE)
+    cartoon_name = models.ForeignKey(Caricature, related_name='fav_cartoon', verbose_name="漫画", help_text="漫画编号", on_delete=models.CASCADE)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
+        unique_together = ("user", "cartoon_name")
+        db_table = 'users_fav'
+
+    def __str__(self):
+        return self.user
+
+
+class UserLeavingMessage(models.Model):
+    """
+    用户评论
+    """
+    user = models.ForeignKey(User, verbose_name="评论的用户", on_delete=models.CASCADE)
+    cartoon = models.ForeignKey(Caricature, verbose_name="评论的漫画", on_delete=models.CASCADE)
+    message = models.TextField(default="", verbose_name="评论内容", help_text="评论内容")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户留言"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.message
--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,6 @@ from datetime import datetime
 # 覆盖默认django自带的用户表
 from django.contrib.auth.models import AbstractUser
 # from django.contrib.auth import get_user_model
-from apps.caricature.models import Caricature
 
 # User = get_user_model()
 
@@ -43,38 +42,3 @@ class UserProfile(AbstractUser):
             return self.nick_name
         else:
             return self.username
-
-
-class UserFav(models.Model):
-    """
-    用户收藏
-    """
-    user = models.ForeignKey(UserProfile, verbose_name="用户", on_delete=models.CASCADE)
-    cartoon_name = models.ForeignKey(Caricature, related_name='fav_cartoon', verbose_name="漫画", help_text="漫画编号", on_delete=models.CASCADE)
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
-
-    class Meta:
-        verbose_name = '用户收藏'
-        verbose_name_plural = verbose_name
-        unique_together = ("user", "cartoon_name")
-        db_table = 'users_fav'
-
-    def __str__(self):
-        return self.user
-
-
-class UserLeavingMessage(models.Model):
-    """
-    用户评论
-    """
-    user = models.ForeignKey(UserProfile, verbose_name="评论的用户", on_delete=models.CASCADE)
-    cartoon = models.ForeignKey(Caricature, verbose_name="评论的漫画", on_delete=models.CASCADE)
-    message = models.TextField(default="", verbose_name="评论内容", help_text="评论内容")
-    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
-
-    class Meta:
-        verbose_name = "用户留言"
-        verbose_name_plural = verbose_name
-
-    def __str__(self):
-        return self.message
